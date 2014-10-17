d2f9fa0f37df535031c42c80bb09429363b9cdfd
Jeffrey Quizon <jeffrey.quizon@gmail.com>
2014-10-17 11:59:39 -0700

    Changed seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,13 +1,24 @@
 u1 = User.create(email: 'ripe@gmail.com', password: 'password')
 u2 = User.create(email: 'thejamaicandave@gmail.com', password: 'password')
 
+u1.save!
+u2.save!
+
 b1 = u1.boards.create(title: 'Workout')
 b2 = u1.boards.create(title: 'Jamaica')
 b3 = u2.boards.create(title: 'Bouncy')
 
+b1.save
+b2.save
+b3.save
+
 l1 = b1.lists.create(title: 'todo')
 l2 = b1.lists.create(title: 'doing')
 l3 = b1.lists.create(title: 'done')
+
+l1.save
+l2.save
+l3.save
 
 c1 = l3.cards.create(title: 'squats', description: 'feel the burn')
 c2 = l3.cards.create(title: 'pushups', description: 'ooh ouch')
@@ -20,6 +31,8 @@ c6 = l1.cards.create(title: 'situps', description: 'ouchy')
 c7 = l2.cards.create(title: 'squats', description: 'feel the burn')
 c8 = l2.cards.create(title: 'pushups', description: 'ooh ouch')
 c9 = l2.cards.create(title: 'situps', description: 'ouchy')
+
+c1.save
 
 i1 = c1.items.create(done: false, title: 'mocha')
 i2 = c1.items.create(done: true, title: 'mocha')
